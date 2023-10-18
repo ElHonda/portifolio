8253f67090eaf4455afa057347aec918e94c22aa
Ernane Helder Rodrigues <ehrodr@yahoo.com.br>
2023-10-18 19:22:09 -0300

    chore: adding comments on js files

--- js/blink-link.js
+++ js/blink-link.js
@@ -1,8 +1,12 @@
+// Executa função no carregamento do DOM
 document.addEventListener("DOMContentLoaded", function() {
+    // Recupera os links do navbar
     const links = document.querySelectorAll("nav a");
 
+    // Para cada link, adiciona evento de click
     links.forEach(link => {
         link.addEventListener("click", function (e) {
+            // Evita chamar a função padrão
             e.preventDefault();
     
             // Obtém o ID da seção vinculada ao link
@@ -13,11 +17,12 @@ document.addEventListener("DOMContentLoaded", function() {
             section.classList.add("selected");
     
             // Aplica temporariamente um filtro de brilho para escurecer
-            section.style.filter = "brightness(0.7)"; // Ajuste conforme desejado
+            section.style.filter = "brightness(0.7)";
         
+            // Corrige o offset do Top subtraindo o tamanho da navbar
             window.scrollTo(0, section.offsetTop - document.getElementsByClassName("navbar")[0].offsetHeight);
     
-            // Remove o filtro após 2 segundos (ajuste conforme desejado)
+            // Remove o filtro após 0.5 segundos
             setTimeout(() => {
                 section.style.filter = "brightness(1)";
             }, 500);
--- js/theme-control.js
+++ js/theme-control.js
@@ -1,25 +1,35 @@
+// Lista de temas disponiveis
 const THEMES = ["light-1", "light-2", "light-3", "light-4", "dark-1", "dark-2", "dark-3"];
 
+// Função para troca de tema
 function changeTheme(newTheme) {
+    // Somente tenta mudar o tema caso ele exista
     if(THEMES.includes(newTheme)){
         // Encontra a tag <link> pelo ID
         var linkTag = document.getElementById("theme-link");
 
+        // Se a tag existir, altera o href com o tema desejado
         if (linkTag) {
             // Modifica o atributo href
             linkTag.setAttribute("href", "css/themes/" + newTheme + "-theme.css" );
             localStorage.setItem("selectedTheme", newTheme);
         }
     }else{
+        // Mensagem de tema não definido
         console.warn("Undefined Theme");
     }
 }
 
+// Adiciona evento na inicialização da página
 document.addEventListener("DOMContentLoaded", function() {
+    // Recupera o tema corrente do localStorage
     let currentTheme = localStorage.getItem("selectedTheme");
+    // Caso seja um tema válido, aplica
     if(THEMES.includes(currentTheme)){
+        // Chama a função para alterar o tema
         changeTheme(currentTheme);
     }else{
+        // Define o tema padrão caso não tenha encontrado um tema válido
         localStorage.setItem("selectedTheme", "dark-1");
     }
 });
